ff9be441ac1e7628126a7142818ad2b1790aca8c
Aarnoud Meijer <aarnoudmeijer@gmail.com>
2019-06-19 16:25:25 +0200

    cleanup functions

--- chatbot/controller/app.php
+++ chatbot/controller/app.php
@@ -2,12 +2,12 @@
 
 namespace chatbot\controller;
 
-/*
+
 // initiate app
 $configs =  [
 	'settings' => ['displayErrorDetails' => true],
 ];
-*/
+
 
 $app = new \Slim\App($configs);
 
@@ -43,75 +43,13 @@ $app->post('/', function ($request, $response){
 		$userMessage = $event['message']['text'];
 
 		$thesender = $event['source']['userId'];
+		file_put_contents('php://stderr', " ----- ");
 
-      file_put_contents('php://stderr', " ----- ");
-
-
-	   if(strtolower($userMessage) == 'ok2'){
-
-		// $data = new \LINE\LINEBot\MessageBuilder\FlexMessageBuilder("Allernatief", $res);
-		  
-		  	$data = FlexSampleRestaurant::get();
-
-          file_put_contents('php://stderr', 'reply data: ' . print_r($data, true));
-
-
-			$result = $bot->replyMessage($event['replyToken'], $data);
-
-			//return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
-
-			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-		
-		}
-
-
-	   if(strtolower($userMessage) == 'wie'){
-			$message = $thesender;
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
-			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-		
-		}
-
-
-        if(strtolower($userMessage) == 'sticker'){
-	
-            $mysticker = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder("11538", "51626501");
-			$result = $bot->replyMessage($event['replyToken'], $mysticker);
-			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-		
-		}
-
-        if(strtolower($userMessage) == 'emo'){
-
-            $message = "Hallo "  . json_decode('"\ud83d\ude00"');
-
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
-			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-		
-		}
-
-
-
-
-
+		require('functions.php');
 
 	}
-	
 
 });
-
-// $app->get('/push/{to}/{message}', function ($request, $response, $args)
-// {
-// 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
-// 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
-
-// 	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($args['message']);
-// 	$result = $bot->pushMessage($args['to'], $textMessageBuilder);
-
-// 	return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-// });
 
 /* JUST RUN IT */
 $app->run();
--- chatbot/controller/functions.php
+++ chatbot/controller/functions.php
@@ -1,20 +1,12 @@
 <?php
 
 if(strtolower($userMessage) == 'ok2'){
-
 // $data = new \LINE\LINEBot\MessageBuilder\FlexMessageBuilder("Allernatief", $res);
-  
   	$data = FlexSampleRestaurant::get();
-
     file_put_contents('php://stderr', 'reply data: ' . print_r($data, true));
-
-
 	$result = $bot->replyMessage($event['replyToken'], $data);
-
 	//return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
-
 	return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-
 }
 
 
@@ -23,25 +15,19 @@ if(strtolower($userMessage) == 'wie'){
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
 	$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
 	return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-
 }
 
 
 if(strtolower($userMessage) == 'sticker'){
-
     $mysticker = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder("11538", "51626501");
 	$result = $bot->replyMessage($event['replyToken'], $mysticker);
 	return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-
 }
 
 if(strtolower($userMessage) == 'emo'){
-
     $message = "Hallo "  . json_decode('"\ud83d\ude00"');
-
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
 	$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
 	return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-
 }
 
